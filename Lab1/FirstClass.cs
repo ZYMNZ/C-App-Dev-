4c565ed7035beea236168cd61c0e40cc84986fb0
Conan <98342638+ZYMNZ@users.noreply.github.com>
2023-01-29 12:49:45 -0500

    First Class

---
+++ Lab1/FirstClass.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace FirstClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my first C# Program!");  
+        }
+    }
+}
